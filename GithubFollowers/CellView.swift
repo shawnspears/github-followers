c9466638c2d8f8b831f3759ef7573f1c948cce4d
shawnspears <shspears@pdx.edu>
2023-06-23 10:40:01 -0400

    Make avatar and label lazy variables

--- GithubFollowers/CellView.swift
+++ GithubFollowers/CellView.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class CellView: UICollectionViewCell {
-    let avatar = createAvatarView()
-    let usernameLabel = createUILabel(fontSize: 16, weight: .bold)
+    lazy var createAvatarView: () -> UIImageView = {
+        let avatar = UIImageView()
+        avatar.layer.cornerRadius = 10
+        avatar.clipsToBounds = true
+        avatar.image = UIImage(named: "placeholder")
+        avatar.translatesAutoresizingMaskIntoConstraints = false
+        return avatar
+    }
+    
+    lazy var createUsernameLabel: (CGFloat, UIFont.Weight) -> UILabel = { fontSize, weight in
+        let usernameLabel = UILabel()
+        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+        usernameLabel.textAlignment = .center
+        usernameLabel.text = "shawnspears"
+        usernameLabel.font = UIFont.systemFont(ofSize: fontSize, weight: weight)
+        return usernameLabel
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        // Create the avatar and username subviews
+        let usernameLabel = createUsernameLabel(16, .bold)
+        let avatar = createAvatarView()
+        
         contentView.addSubview(avatar)
         contentView.addSubview(usernameLabel)
     }
@@ -21,22 +41,4 @@ class CellView: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-}
-
-private func createUILabel(fontSize: CGFloat, weight: UIFont.Weight) -> UILabel {
-    let usernameLabel = UILabel()
-    usernameLabel.translatesAutoresizingMaskIntoConstraints = false
-    usernameLabel.textAlignment = .center
-    usernameLabel.text = "shawnspears"
-    usernameLabel.font = UIFont.systemFont(ofSize: fontSize, weight: weight)
-    return usernameLabel
-}
-
-private func createAvatarView() -> UIImageView {
-    let avatar = UIImageView()
-    avatar.layer.cornerRadius = 10
-    avatar.clipsToBounds = true
-    avatar.image = UIImage(named: "placeholder")
-    avatar.translatesAutoresizingMaskIntoConstraints = false
-    return avatar
 }
